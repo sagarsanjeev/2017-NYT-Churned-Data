fa718935833ecf00d5cb1b60c4e6f1a1aa6e9529
Sagar Sanjeev <sagarsanjeev24@users.noreply.github.com>
2018-12-06 00:32:46 +0530

    Update

--- sort.py
+++ sort.py
@@ -16,13 +16,13 @@ def churn(url):
 
 def ner():
     output_file = '/home/rockerzsag24/Ready/1.txt'
-    input = open('/home/rockerzsag24/Ready/Data/1.txt', 'r').read()
+    input = open('/home/rockerzsag24/Ready/Data/2.txt', 'r').read()
     input = input.replace('\t', '\n')
     input = input.split('News')
     input = list(map(lambda s: s.strip(), input))
-    i = 0
+    i = 1001
     for article in input:
-        output_file = '/home/rockerzsag24/Ready/1/News_' + str(i) + '.json'
+        output_file = '/home/rockerzsag24/Ready/2/News_' + str(i) + '.json'
         if article:
             try:
                 article_triplet = article.split(' ', 1)
